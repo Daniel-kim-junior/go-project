6d66ec8a325bb7ffa74276c4aa6d012d4a0d1b5a
jiokim-7 <now20412041@gmail.com>
2023-12-23 16:13:51 +0900

    add : linkedlist struct and basic methods

--- linkedlist/singlelinkedlist.go
+++ linkedlist/singlelinkedlist.go
@@ -1,5 +1,42 @@
 package singlelinkedlist
 
-func MyModTest() string {
-	return "This is from MyModTest in my_mod"
+type Node[T any] struct {
+	next *Node[T]
+	val  T
+}
+
+type LinkedList[T any] struct {
+	rootNode *Node[T]
+	tailNode *Node[T]
+}
+
+func (l *LinkedList[T]) PushBack(value T) {
+	node := &Node[T]{nil, value}
+	if l.rootNode == nil {
+		l.rootNode = node
+		l.tailNode = node
+		return
+	}
+
+	l.tailNode.next = node
+	l.tailNode = node
+}
+
+func (l *LinkedList[T]) PushFront(value T) {
+	node := &Node[T]{val: value}
+	if l.rootNode == nil {
+		l.rootNode = node
+		l.tailNode = node
+		return
+	}
+	node.next = l.rootNode
+	l.rootNode = node
+}
+
+func (l *LinkedList[T]) Front() *Node[T] {
+	return l.rootNode
+}
+
+func (l *LinkedList[T]) Back() *Node[T] {
+	return l.tailNode
 }
