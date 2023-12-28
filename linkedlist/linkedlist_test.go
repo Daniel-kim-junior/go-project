cf7cf8a93c175e50326468893b3b436c4a94125e
jiokim-7 <now20412041@gmail.com>
2023-12-28 22:18:35 +0900

    add : linkedlist insert method

--- linkedlist/linkedlist_test.go
+++ linkedlist/linkedlist_test.go
@@ -21,3 +21,48 @@ func TestPushBack(t *testing.T) {
 	assert.Equal(t, 1, l.Front().val)
 	assert.Equal(t, 2, l.Back().val)
 }
+
+func TestPushFront(t *testing.T) {
+	var l LinkedList[int]
+	l.PushFront(1)
+	assert.NotNil(t, l.rootNode)
+	assert.NotNil(t, l.tailNode)
+	assert.Equal(t, 1, l.Front().val)
+	assert.Equal(t, 1, l.Back().val)
+	l.PushFront(2)
+	assert.Equal(t, 2, l.Front().val)
+	assert.Equal(t, 1, l.Back().val)
+}
+
+func TestSize(t *testing.T) {
+	var l LinkedList[int]
+	l.PushFront(1)
+	l.PushBack(1)
+	l.PushFront(2)
+	assert.Equal(t, 3, l.Size())
+}
+
+func TestSize2(t *testing.T) {
+	var l LinkedList[int]
+	l.PushFront(1)
+	assert.Equal(t, 1, l.Size2())
+	assert.Equal(t, 1, l.GetAt(0).val)
+	assert.Nil(t, l.GetAt(1))
+}
+
+func TestInsertAfter(t *testing.T) {
+	var l LinkedList[int]
+	l.PushBack(1)
+	l.PushBack(2)
+	l.PushBack(3)
+
+	l.InsertAfter(l.GetAt(1), 7)
+	assert.Equal(t, 4, l.Size2())
+	assert.Equal(t, 7, l.GetAt(2).val)
+	assert.Equal(t, 3, l.tailNode.val)
+
+	notExist := &Node[int]{val: 100}
+	l.InsertAfter(notExist, 8)
+	assert.Equal(t, 4, l.Size2())
+
+}
--- linkedlist/singlelinkedlist.go
+++ linkedlist/singlelinkedlist.go
@@ -8,10 +8,12 @@ type Node[T any] struct {
 type LinkedList[T any] struct {
 	rootNode *Node[T]
 	tailNode *Node[T]
+	count    int
 }
 
 func (l *LinkedList[T]) PushBack(value T) {
 	node := &Node[T]{nil, value}
+	l.count++
 	if l.rootNode == nil {
 		l.rootNode = node
 		l.tailNode = node
@@ -24,6 +26,7 @@ func (l *LinkedList[T]) PushBack(value T) {
 
 func (l *LinkedList[T]) PushFront(value T) {
 	node := &Node[T]{val: value}
+	l.count++
 	if l.rootNode == nil {
 		l.rootNode = node
 		l.tailNode = node
@@ -39,4 +42,55 @@ func (l *LinkedList[T]) Front() *Node[T] {
 
 func (l *LinkedList[T]) Back() *Node[T] {
 	return l.tailNode
+}
+
+func (l *LinkedList[T]) Size() int {
+	node := l.rootNode
+	var count int = 0
+
+	for ; node != nil; node = node.next {
+		count++
+	}
+
+	return count
+}
+
+func (l *LinkedList[T]) Size2() int {
+	return l.count
+}
+
+func (l *LinkedList[T]) GetAt(idx int) *Node[T] {
+	if idx >= l.Size2() {
+		return nil
+	}
+
+	i := 0
+	for node := l.rootNode; node != nil; node = node.next {
+		if i == idx {
+			return node
+		}
+		i++
+	}
+	return nil
+}
+
+func (l *LinkedList[T]) InsertAfter(node *Node[T], value T) {
+	if !l.isIncluded(node) {
+		return
+	}
+	newNode := &Node[T]{nil, value}
+	node.next, newNode.next = newNode, node.next
+	l.count++
+}
+
+func (l *LinkedList[T]) isIncluded(node *Node[T]) bool {
+	inner := l.rootNode
+
+	for ; inner != nil; inner = inner.next {
+		if inner == node {
+			return true
+		}
+	}
+
+	return false
 }
