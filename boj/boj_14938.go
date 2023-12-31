2d9e60020f4eede4e5232788d5142398ce09e312
jiokim-7 <now20412041@gmail.com>
2024-01-01 02:05:32 +0900

    add : boj_14938

--- boj/boj_14938.go
+++ boj/boj_14938.go
@@ -6,9 +6,71 @@ import (
 	"os"
 	"strconv"
 	"strings"
-
-	ds "github.com/Daniel-kim-junior/go-project/godatasturct"
 )
+
+type HeapNode[T Comparable[T]] struct {
+	Val T
+}
+type Comparable[T any] interface {
+	Compare(o T) bool
+}
+type Heap[T Comparable[T]] struct {
+	Array []HeapNode[T]
+	Size  int
+}
+
+func (h *Heap[T]) Insert(Value T) {
+	node := HeapNode[T]{Val: Value}
+	if h.Array == nil {
+		h.Array = make([]HeapNode[T], 0)
+	}
+	h.Array = append(h.Array, node)
+	h.Size = h.Size + 1
+	h.HeapifyUp(h.Size)
+}
+
+func (h *Heap[T]) HeapifyUp(tail int) {
+	cur := tail
+	for ; cur/2 != 0 && h.Array[cur/2-1].Val.Compare(h.Array[cur-1].Val); cur /= 2 {
+		h.Array[cur/2-1], h.Array[cur-1] = h.Array[cur-1], h.Array[cur/2-1]
+	}
+}
+
+func Zero[T Comparable[T]]() T {
+	return *new(T)
+}
+
+func (h *Heap[T]) Pop() *HeapNode[T] {
+	if h.Size == 0 {
+		return nil
+	}
+	node := h.Array[0]
+	h.Size--
+	if h.Size == 0 {
+		h.Array = h.Array[1:]
+	} else {
+		h.Array[0] = h.Array[len(h.Array)-1]
+		h.Array = h.Array[:len(h.Array)-1]
+		h.HeapifyDown()
+	}
+	return &node
+}
+
+func (h *Heap[T]) HeapifyDown() {
+	for curIdx := 1; curIdx*2 < h.Size; {
+		if !h.Array[curIdx-1].Val.Compare(h.Array[curIdx*2-1].Val) && !h.Array[curIdx-1].Val.Compare(h.Array[curIdx*2].Val) {
+			break
+		}
+		if h.Array[curIdx-1].Val.Compare(h.Array[curIdx*2-1].Val) {
+			h.Array[curIdx-1], h.Array[curIdx*2-1] = h.Array[curIdx*2-1], h.Array[curIdx-1]
+			curIdx *= 2
+			curIdx -= 1
+		} else {
+			h.Array[curIdx-1], h.Array[curIdx*2] = h.Array[curIdx*2], h.Array[curIdx-1]
+			curIdx *= 2
+		}
+	}
+}
 
 var n int
 var m int
@@ -22,7 +84,7 @@ type GraphNode struct {
 }
 
 func (g GraphNode) Compare(o GraphNode) bool {
-	return g.itemCost > o.itemCost
+	return g.cost < o.cost
 }
 
 func main() {
@@ -43,10 +105,10 @@ func main() {
 	var from int
 	var to int
 	var cost int
-	graph := make([][]GraphNode, n+1)
-	row := make([]GraphNode, (n+1)*(n+1))
+	graph := make([][]int, n+1)
+	row := make([]int, (n+1)*(n+1))
 	for i := 1; i <= n; i++ {
-		graph[i] = row[i*n : (i+1)*n]
+		graph[i] = row[i*(n+1) : (i+1)*(n+1)]
 	}
 
 	for i := 0; i < r; i++ {
@@ -55,42 +117,46 @@ func main() {
 		from, _ = strconv.Atoi(input[0])
 		to, _ = strconv.Atoi(input[1])
 		cost, _ = strconv.Atoi(input[2])
-		graph[from] = append(graph[from], GraphNode{to, cost, 0})
-		graph[to] = append(graph[to], GraphNode{from, cost, 0})
+		graph[from][to] = cost
+		graph[to][from] = cost
+	}
+	for i := 1; i <= n; i++ {
+		BFS(i, graph, items)
 	}
 
-	for i := 1; i <= n; i++ {
-		Dijkstra(i, graph, items)
-	}
 	fmt.Print(rst)
 
 }
-func Dijkstra(cur int, graph [][]GraphNode, items []int) {
-	heap := &ds.Heap[GraphNode]{make([]ds.HeapNode[GraphNode], 0), 0}
+func BFS(cur int, graph [][]int, items []int) {
+	heap := &Heap[GraphNode]{Size: 0}
 	DP := make([]int, n+1)
-	for i := 0; i <= n; i++ {
-		DP[i] = 1234567890
+	var INF int = 123456789
+	for i := 1; i <= n; i++ {
+		DP[i] = INF
 	}
 	DP[cur] = 0
-	heap.Insert(GraphNode{cur, 0, items[cur]})
-	var NList []GraphNode
-	var Nnode GraphNode
+	l := 0
+	heap.Insert(GraphNode{cur, 0, 0})
 	for heap.Size != 0 {
-		node := *heap.Pop()
-		NList = graph[node.Val.idx]
-		if node.Val.cost > m {
-			continue
-		}
-		if rst < node.Val.itemCost {
-			rst = node.Val.itemCost
-		}
-		for i := 0; i < len(graph[node.Val.idx]); i++ {
-			Nnode = NList[i]
-			if DP[Nnode.idx] > node.Val.cost+Nnode.cost {
-				DP[Nnode.idx] = node.Val.cost + Nnode.cost
-				heap.Insert(GraphNode{Nnode.idx, DP[Nnode.idx], node.Val.itemCost + items[Nnode.idx]})
+		node := heap.Pop()
+
+		for i := 1; i <= n; i++ {
+			if node.Val.cost+graph[node.Val.idx][i] > m {
+				continue
+			}
+			if DP[i] > graph[node.Val.idx][i]+node.Val.cost && graph[node.Val.idx][i] != 0 {
+				DP[i] = graph[node.Val.idx][i] + node.Val.cost
+				heap.Insert(GraphNode{i, node.Val.cost + graph[node.Val.idx][i], 0})
 			}
 		}
 	}
-
+	for i := 1; i <= n; i++ {
+		if DP[i] == INF {
+			continue
+		}
+		l += items[i]
+	}
+	if l > rst {
+		rst = l
+	}
 }
