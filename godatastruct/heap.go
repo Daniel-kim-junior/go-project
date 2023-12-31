ccde6721ef6e4fca38bacec164037c297ade4025
jiokim-7 <now20412041@gmail.com>
2023-12-31 15:19:09 +0900

    fix : heap datastruct heapify

--- godatastruct/heap.go
+++ godatastruct/heap.go
@@ -1,65 +1,57 @@
 package godatastruct
 
-type HeapNode[T comparable[T]] struct {
+type HeapNode[T Comparable[T]] struct {
 	val T
 }
-type comparable[T any] interface {
-	compare(o T) bool
+type Comparable[T any] interface {
+	Compare(o T) bool
 }
-type Heap[T comparable[T]] struct {
-	Array   []HeapNode[T]
-	Size    int
-	TailIdx int
+type Heap[T Comparable[T]] struct {
+	Array []HeapNode[T]
+	Size  int
 }
 
-func (h Heap[T]) Insert(value T) {
+func (h *Heap[T]) Insert(value T) {
 	node := &HeapNode[T]{val: value}
-
-	var tail int
-	if h.Size%2 == 0 {
-		h.Array[h.Size+1] = *node
-		tail = h.Size + 1
-	} else {
-		h.Array[2*(h.Size-1)] = *node
-		tail = 2 * (h.Size - 1)
-	}
-	h.Size++
-	h.HeapifyUp(tail)
-	h.TailIdx = tail
+	h.Array[h.Size+1] = *node
+	h.Size = h.Size + 1
+	h.HeapifyUp(h.Size + 1)
 }
 
-func (h Heap[T]) HeapifyUp(tail int) {
+func (h *Heap[T]) HeapifyUp(tail int) {
 	cur := tail
 	tailVal := h.Array[tail]
-	for ; cur/2 != 0 && tailVal.val.compare(h.Array[cur].val); cur /= 2 {
+	for ; cur/2 != 0 && tailVal.val.Compare(h.Array[cur].val); cur /= 2 {
 		h.Array[cur/2], tailVal = tailVal, h.Array[cur]
 	}
 }
 
-func (h Heap[T]) Pop() *HeapNode[T] {
+func Zero[T Comparable[T]]() T {
+	return *new(T)
+}
+
+func (h *Heap[T]) Pop() *HeapNode[T] {
+	if h.Size == 0 {
+		return nil
+	}
 	node := h.Array[1]
-	h.Array[1] = h.Array[h.TailIdx]
+	h.Array[1], h.Array[h.Size].val = h.Array[h.Size], Zero[T]()
+	h.Size--
 	h.HeapifyDown()
 	return &node
 }
 
-func (h Heap[T]) HeapifyDown() {
-	curIdx := 1
-	for true {
-		if !h.Array[curIdx*2].val.compare(h.Array[curIdx].val) && !h.Array[curIdx*2+1].val.compare(h.Array[curIdx].val) {
+func (h *Heap[T]) HeapifyDown() {
+	for curIdx := 1; curIdx*2 < h.Size; {
+		if !h.Array[curIdx].val.Compare(h.Array[curIdx*2].val) && !h.Array[curIdx].val.Compare(h.Array[curIdx*2+1].val) {
 			break
 		}
-		if h.Array[curIdx*2].val.compare(h.Array[curIdx].val) {
-			h.Array[curIdx*2], h.Array[curIdx] = h.Array[curIdx], h.Array[curIdx*2]
+		if h.Array[curIdx].val.Compare(h.Array[curIdx*2].val) {
+			h.Array[curIdx], h.Array[curIdx*2] = h.Array[curIdx*2], h.Array[curIdx]
 			curIdx *= 2
-			continue
-		}
-
-		if h.Array[curIdx*2+1].val.compare(h.Array[curIdx].val) {
-			h.Array[curIdx*2+1], h.Array[curIdx] = h.Array[curIdx], h.Array[curIdx*2+1]
+		} else {
+			h.Array[curIdx], h.Array[curIdx*2+1] = h.Array[curIdx*2+1], h.Array[curIdx]
 			curIdx = curIdx*2 + 1
 		}
-
 	}
-	h.TailIdx = curIdx
 }
--- godatastruct/heap_test.go
+++ godatastruct/heap_test.go
@@ -10,13 +10,22 @@ type CompareInt struct {
 	val int
 }
 
-func (c CompareInt) compare(o CompareInt) bool {
+func (c CompareInt) Compare(o CompareInt) bool {
 	return o.val < c.val
 }
 
 func TestHeap(t *testing.T) {
-	heap := &Heap[CompareInt]{make([]HeapNode[CompareInt], 10), 0, 0}
-	assert.Equal(t, CompareInt{0}, heap.Pop().val)
-
-	// assert.Equal(t, CompareInt(10), heap.Pop().val)
+	heap := &Heap[CompareInt]{make([]HeapNode[CompareInt], 10), 0}
+	assert.Nil(t, heap.Pop())
+	heap.Insert(CompareInt{10})
+	assert.Equal(t, 1, heap.Size)
+	assert.Equal(t, CompareInt{10}, heap.Pop().val)
+	heap.Insert(CompareInt{1})
+	heap.Insert(CompareInt{7})
+	heap.Insert(CompareInt{6})
+	heap.Insert(CompareInt{1})
+	assert.Equal(t, CompareInt{1}, heap.Pop().val)
+	assert.Equal(t, CompareInt{1}, heap.Pop().val)
+	assert.Equal(t, CompareInt{6}, heap.Pop().val)
+	assert.Equal(t, CompareInt{7}, heap.Pop().val)
 }
